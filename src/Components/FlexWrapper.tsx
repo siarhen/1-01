ac30d930b29287cf8b2d723775e0fa853e642f74
Daniil Shyshayeu <daniil.shishaev@gmail.com>
2024-12-03 13:59:04 +0300

    adding components and flexWrapper rsc

---
+++ src/Components/FlexWrapper.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+type FlexWrapperPropsType = {
+    direction?: string;
+    justify?: string;
+    alignItems?: string;
+    wrap?: string;
+}
+export const FlexWrapper = styled.div<FlexWrapperPropsType>`
+    display: flex;
+    flex-direction: ${props => props.direction || "row"};
+    justify-content: ${props => props.justify || "flex-start"};
+    align-items: ${props => props.alignItems || "stretch"};
+    flex-wrap: ${props => props.wrap || "nowrap"};
+`
+
